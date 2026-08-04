911785212a938934357a19cb5839dd455a7f9d38
idarosej <idarosej123@gmail.com>
2026-08-04 18:09:13 +0530

    Day 13: Improved UI with confidence and gesture history

--- static/js/script.js
+++ static/js/script.js
@@ -37,6 +37,23 @@ async function updateHistory() {
 
 }
 
+async function updateConfidence(){
+
+    const response = await fetch("/confidence");
+
+    const data = await response.json();
+
+    document.getElementById("confidenceText").innerText =
+        data.confidence + "%";
+
+    document.getElementById("confidenceBar").style.width =
+        data.confidence + "%";
+
+}
+
+setInterval(updateConfidence,500);
+
+
 setInterval(updateHistory, 1000);
 
 // Update immediately
